88ecaa48e77653b44979fceb059a035fadb8b059
Keishi Masuda <ke-masuda@kentem.co.jp>
2024-06-28 10:20:31 +0900

    第二問　貸し出し状況・削除機能追加

--- my-book-register/src/App.tsx
+++ my-book-register/src/App.tsx
@@ -5,6 +5,10 @@ import { BookItemModel } from './models';
 import BookRegister from './components/bookRegister';
 
 function App() {
+
+  //9784297130589
+  //9784873115658
+
   const [books, setBooks] = useState<BookItemModel[]>([]);
 
   const handleClickButton = (isbn : string): void => {
@@ -41,11 +45,16 @@ function App() {
       <FilterableBookTable
         books={books}
         onClickDelete={(id) => {
-            {/* 第2問：貸出 or 返却 or 削除の処理を追加 */}            
+            const newBooks = books.filter(bookItems=>bookItems.id !== id)
+            console.log(newBooks)
+            setBooks(newBooks)             
           }
         }
         onClickLendingSwitch={(id) => {
-            {/* 第2問：貸出 or 返却 or 削除の処理を追加 */}            
+          //mapでつくれるか・・・・
+          const newBooks = [...books]
+          newBooks[Number(id)].isOnLoan = !newBooks[Number(id)].isOnLoan
+          setBooks(newBooks)
           }
         }
       />
