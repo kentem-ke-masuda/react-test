6b9a906336c4cb4e6af8d0f27a1ea25273288484
Keishi Masuda <ke-masuda@kentem.co.jp>
2024-06-28 12:03:15 +0900

    貸し出し状況によるフィルターの追加(実装途中)

--- my-book-register/src/components/filterableBookTable.tsx
+++ my-book-register/src/components/filterableBookTable.tsx
@@ -15,17 +15,52 @@ const FilterableBookTable = ({
   onClickLendingSwitch,
 }: Props) => {
   const [filterText, setFilterText] = useState('');
-
+  const [radioOption , setRadioOption] = useState("all")
+  
   const handleChangeFilterText: ChangeEventHandler<HTMLInputElement> = (e) =>
     setFilterText(e.target.value);
 
+  const onCheck = (event : React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    setRadioOption(event.target.value)
+  }
+
+  const filBooks = (books : BookItemModel[])=> {
+    if (radioOption == "isLoanAble"){
+      return books.filter( (book) => {!book.isOnLoan})
+    }else if (radioOption == "notIsLoanAble") {
+      return books.filter( (book) => {book.isOnLoan})
+    }else{
+      return [...books]
+    }
+  }
+
   return (
     <div className="filterable-book-table">
-      <LabelInput label="filter" handleEvent={handleChangeFilterText}/>
+      <div className='filter-field'>
+        <LabelInput label="filter" handleEvent={handleChangeFilterText}/>
+        <fieldset className='field'>
+          <legend>貸し出し状況</legend>
+          <div>
+            <input type="radio" id='all' name='isLoanAble' value = "all" defaultChecked
+            onChange={(e)=>{onCheck(e)}}
+            /><label htmlFor="all">全て</label>
+          </div>
+          <div>
+            <input type="radio" id='loanAble'name='isLoanAble' value = "LoanAble"
+            onChange={(e)=>{onCheck(e)}}/><label htmlFor="loanAble">利用可能</label>
+          </div>
+          <div>
+            <input type="radio" id='notLoanAble' name='isLoanAble' value = "notLoanAble"
+            onChange={(e)=>{onCheck(e)}}/><label htmlFor="notLoanAble">貸出中</label>
+          </div>
+        </fieldset>
+      </div>
       <BookTable
-        bookItems={books.filter(
+        bookItems={filBooks(books).filter(
           (x) => !filterText || x.name.includes(filterText),
-        )}
+        )
+      }
         onClickDelete={onClickDelete}
         onClickLendingSwitch={onClickLendingSwitch}
       />
